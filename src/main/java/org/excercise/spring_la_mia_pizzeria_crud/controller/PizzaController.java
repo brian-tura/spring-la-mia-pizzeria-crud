85420eaafed6359517c3d833448d02e9685f2366
brian-tura <b.tura2002@gmail.com>
2025-08-28 12:09:04 +0200

    [DONE] Excercise done

--- src/main/java/org/excercise/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/excercise/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.*;
 
@@ -24,5 +25,13 @@ public class PizzaController {
 
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("book", repository.findById(id).get());
+        Pizza pizza = repository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "/pizzas/show";
     }
 }
